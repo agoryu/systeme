c680c4207884f1b35ecbf7fee426a2fd016ac9f2
GaetanDeflandre <gaetan.deflandre@gmail.com>
2014-09-21 16:09:43 +0200

    fonction de display_stack.c écrite

--- tp1/display_stack.c
+++ tp1/display_stack.c
@@ -1,9 +1,44 @@
 #include <sdtio.h>
 #include <stdlib.h>
 
+
+/**
+ * La fonction bar ne fait oas grand chose,
+ * exemple pour afficher la pile des variables.
+ */
+char bar(int index){
+
+  char string[50] = "bonjour";
+  int indexbis = 0;
+
+  if (index<0 || index>50){
+    indexbis =0;
+  } else {
+    indexbis = index++;
+  }
+
+  return string[indexbis];
+}
+
+/**
+ * La fonction foo ne fait oas grand chose,
+ * exemple pour afficher la pile des variables.
+ */
+int foo(int x, char c){
+
+  int a = 42;
+  int i = 0;
+
+  char bares = bar(i);
+
+  return (int)c + x - a;
+}
+
+
 int main(int argc, char* argv[]){
 
-  // utilisé pointeur esp et ebp
+  int x = 50;
+  int c = 'a';
 
-  return EXIT_SUCCESS;
+  return foo(x,c);
 }
