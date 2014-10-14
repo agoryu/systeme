a27edff2581a3459fa317578c10dbe378f439438
GaetanDeflandre <gaetan.deflandre@gmail.com>
2014-10-14 17:43:11 +0200

    ajour des constante

--- tp5/drive.h
+++ tp5/drive.h
@@ -1,6 +1,14 @@
 #ifndef DRIVE_H
 #define DRIVE_H
 
-
+#define SEEK   0x02
+#define READ   0x04
+#define WRITE  0x6
+#define FORMAT 0x08
+#define STATUS 0x12
+#define DMASET 0x14
+#define DSKNFO 0x16
+#define MANUF  0xA2
+#define DIAG   0xA4
 
 #endif
