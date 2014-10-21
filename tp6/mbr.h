7d3c64477826d0ae03fdf14262437efccc233a84
Vanegue Elliot <vanegue@a12p4.fil.univ-lille1.fr>
2014-10-21 18:36:42 +0200

    ajout des fonctions de lectur, ecritur et formatage

--- tp6/mbr.h
+++ tp6/mbr.h
@@ -4,6 +4,13 @@
 #include "drive.h"
 
 #define MAX_VOL 8
+/**
+ * \brief Define permettant de reconnaitre le contexte en cours 
+ */
+#define CTX_MAGIC 0xCAFEBABE
+
+#define SECTOR_SIZE 8
+#define MAX_SECTORE 10
 
 enum vol_type_e {VOLT_PR, VOLT_SND, VOLT_OTHER}
 
@@ -16,6 +23,9 @@ struct vol-s {
 
 struct mbr_s {
   struct vol_s mbr_vol[MAX_VOL];
+  unsigned mbr_n_vol;
+  unsigned mbr_magic;
+}
   
 
 #endif
